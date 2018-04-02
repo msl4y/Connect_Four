b44d625d77bfa2210e2d9fa734ffd793b5a05244
KadeWalter <kadewalter1@gmail.com>
2018-04-02 17:47:16 -0500

    Newest Update
    Project is almost done. Need to add the following:
    
    ~Greedy Algorithm for hard AI
    ~Let users create and set time limits
    ~Let users access the console window to display the past moves

---
+++ Connect_Four/GameBoard.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Connect_Four
+{
+    partial class GameBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameBoard));
+            this.consoleShow_button = new System.Windows.Forms.Button();
+            this.toMainMenu_button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.startGame_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // consoleShow_button
+            // 
+            this.consoleShow_button.Location = new System.Drawing.Point(144, 12);
+            this.consoleShow_button.Name = "consoleShow_button";
+            this.consoleShow_button.Size = new System.Drawing.Size(117, 29);
+            this.consoleShow_button.TabIndex = 0;
+            this.consoleShow_button.Text = "Display Console";
+            this.consoleShow_button.UseVisualStyleBackColor = true;
+            this.consoleShow_button.Click += new System.EventHandler(this.consoleShow_click);
+            // 
+            // toMainMenu_button
+            // 
+            this.toMainMenu_button.Location = new System.Drawing.Point(276, 12);
+            this.toMainMenu_button.Name = "toMainMenu_button";
+            this.toMainMenu_button.Size = new System.Drawing.Size(117, 29);
+            this.toMainMenu_button.TabIndex = 1;
+            this.toMainMenu_button.Text = "Exit to Main Menu";
+            this.toMainMenu_button.UseVisualStyleBackColor = true;
+            this.toMainMenu_button.Click += new System.EventHandler(this.toMainMenu_click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            // 
+            // startGame_button
+            // 
+            this.startGame_button.Location = new System.Drawing.Point(12, 12);
+            this.startGame_button.Name = "startGame_button";
+            this.startGame_button.Size = new System.Drawing.Size(117, 29);
+            this.startGame_button.TabIndex = 2;
+            this.startGame_button.Text = "Start Game";
+            this.startGame_button.UseVisualStyleBackColor = true;
+            this.startGame_button.Click += new System.EventHandler(this.startGame_click);
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.startGame_button);
+            this.Controls.Add(this.toMainMenu_button);
+            this.Controls.Add(this.consoleShow_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GameBoard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connect Four";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameBoard_FormClosing);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameBoard_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GameBoard_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button consoleShow_button;
+        private System.Windows.Forms.Button toMainMenu_button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button startGame_button;
+    }
+}
---
+++ Connect_Four/MainMenu.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect_Four
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void HumanVHuman_click(object sender, EventArgs e)
+        {
+            // user can choose a game size
+            size s = new size();
+            s.ShowDialog();
+            int game_size = size.SetTheSize;
+            int rows = getRows(game_size);
+            int cols = getCols(game_size);
+            s.Hide();
+
+            if (game_size != 0)
+            {
+                // make the gameboard and start playing
+                GameBoard game = new GameBoard(2, 0, 0, rows, cols);
+                if (game_size == 1)
+                    game.Size = new Size(600, 450);
+                else if (game_size == 3)
+                    game.Size = new Size(1200, 900);
+                game.Show();
+                this.Hide();
+            }
+        }
+
+        private void HumanVComp_click(object sender, EventArgs e)
+        {
+            // user chooses computer difficulty
+            difficulty d = new difficulty();
+            d.ShowDialog();
+            int game_difficulty = difficulty.SetTheDifficulty;
+
+
+            // user can choose a game size
+            size s = new size();
+            s.ShowDialog();
+            int game_size = size.SetTheSize;
+            int rows = getRows(game_size);
+            int cols = getCols(game_size);
+            s.Hide();
+
+            // if the menus weren't closed
+            if (game_size != 0 && game_difficulty != 0)
+            {
+                // make the gameboard and start playing
+                GameBoard game = new GameBoard(1, game_difficulty, 0, rows, cols);
+                if (game_size == 1)
+                    game.Size = new Size(600, 450);
+                else if (game_size == 3)
+                    game.Size = new Size(1200, 900);
+                game.Show();
+                this.Hide();
+            }
+        }
+
+        private void CompVComp_click(object sender, EventArgs e)
+        {
+            int AIone_diff = 0;
+            int AItwo_diff = 0;
+
+            // user chooses computer AI #1 difficulty
+            difficulty AIone = new difficulty();
+            AIone.ShowDialog();
+            AIone_diff = difficulty.SetTheDifficulty;
+
+            // user chooses computer AI #2 difficulty
+            difficulty AItwo = new difficulty();
+            AItwo.ShowDialog();
+            AItwo_diff = difficulty.SetTheDifficulty;
+
+            // user can choose a game size
+            size s = new size();
+            s.ShowDialog();
+            int game_size = size.SetTheSize;
+            int rows = getRows(game_size);
+            int cols = getCols(game_size);
+            s.Hide();
+
+            // if the menus weren't closed
+            if (game_size != 0 && AIone_diff != 0 && AItwo_diff != 0)
+            {
+                // make the gameboard and start playing
+                GameBoard game = new GameBoard(0, AIone_diff, AItwo_diff, rows, cols);
+                if (game_size == 1)
+                    game.Size = new Size(600, 450);
+                else if (game_size == 3)
+                    game.Size = new Size(1200, 900);
+                game.Show();
+            }
+        }
+
+        private void Exit_click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private int getRows(int gameSize)
+        {
+            if (gameSize == 1)
+                return 3;
+            else if (gameSize == 2)
+                return 6;
+            else
+                return 9;
+        }
+
+        private int getCols(int gameSize)
+        {
+            if (gameSize == 1)
+                return 4;
+            else if (gameSize == 2)
+                return 7;
+            else
+                return 10;
+        }
+
+    }
+}
